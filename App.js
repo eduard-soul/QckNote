66d6b29b2b32f4951e6c20be1b987256477118dc
Eduard <eduard.tataru@mailo.com>
2023-11-02 10:06:37 +0100

    done initial code for card of notes

--- App.js
+++ App.js
@@ -1,185 +1,293 @@
 import { useEffect, useState } from 'react';
-import { Dimensions } from 'react-native';
-import { Platform, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
-import { Keyboard } from 'react-native';
+import { KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, View, Dimensions, Keyboard, useWindowDimensions, Image } from 'react-native';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 import { useFonts } from 'expo-font';
+import { setStatusBarBackgroundColor } from 'expo-status-bar';
+import { BlurView } from 'expo-blur';
 
-// I AM BUILDING A APP FOR IOS/ANDROID 
-// THE ROLE OF THE APP : MINIMIZE THE MORE POSSIBLE THE FRICTION TO WRITE AN IDEA/A THOUGHT
-
-import { KeyboardAvoidingView } from 'react-native';
+// REACT NATIVE APP
 
 export default function App() {
-  const [loaded] = useFonts({
-    GeistVF: require('./assets/fonts/GeistVF.ttf'),
-    GeistBlack: require('./assets/fonts/Geist-Black.otf'),
-    GeistBold: require('./assets/fonts/Geist-Bold.otf'),
-    GeistLight: require('./assets/fonts/Geist-Light.otf'),
-    GeistMedium: require('./assets/fonts/Geist-Medium.otf'),
-    GeistRegular: require('./assets/fonts/Geist-Regular.otf'),
-    GeistSemiBold: require('./assets/fonts/Geist-SemiBold.otf'),
-    GeistThin: require('./assets/fonts/Geist-Thin.otf'),
-    GeistUltraBlack: require("./assets/fonts/Geist-UltraBlack.otf"),
-    GeistUltraLight: require("./assets/fonts/Geist-UltraLight.otf"),
-  })
+    const DARK_BACKGROUD = '#222120';
+    const LIGHT_BACKGROUND = '#FFF9F3';
 
-  const [noteText, setNoteText] = useState('');
-  const [headerText, setHeaderText] = useState('');
-  const [headerHeight, setHeaderHeight] = useState(Dimensions.get(Platform.OS === 'web' ? "window" : "screen").height * 0.1);
+    const DARK_CARD = '#101010';
+    const LIGHT_CARD = '#ECECEC';
 
-  const scale = useSharedValue(1);
+    const DARK_LOGO = '#0085FF';
+    const LIGHT_LOGO = '#001AFF';
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: scale.value }]
-    }
-  })
+    const DARK_NOTE_TITLE = '#E6E6E6';
+    const LIGHT_NOTE_TITLE = '#1C1C1C';
 
-  function buttonAnimation() {
-    scale.value = withSpring(1);
-  }
+    const DARK_NOTE_TITLE_PLACEHOLDER = '#F2F2F2';
+    const LIGHT_NOTE_TITLE_PLACEHOLDER = '#D6D6D6';
 
-  return (
-    <View style={styles.appContainer}>
-        <View style={[styles.headerSettingsButtonWrapper, 
-              {height: headerHeight,
-                backgroundColor: 'transparent',
-                alignItems: 'center',
-              },
-              {marginTop: Platform.OS === 'web' ? '0%' : '11%'}
-        ]}>
-          <TextInput 
-            style={[
-              styles.inputHeader, 
-              Platform.OS === 'web' ? {outlineStyle: 'none'} : {},
-              {
-                borderStyle: 'solid',
-                borderColor: 'rgba(0,0,100,0.1)',
-                borderWidth: 1.5,
-                borderRadius: 10,
-                fontFamily: 'GeistSemiBold',
-              }
-            ]}
-            value={headerText}
-            onChangeText={setHeaderText}
-            placeholder='Optional title'
-            placeholderTextColor={'rgba(0,0,0,0.2)'}
-          ></TextInput>
-      </View>
-      <View style={{
-          height: '90%',
-          width: '100%',
-      }}>
-        <KeyboardAvoidingView style={{
-          height: '100%',
-          width: '100%',
-          alignItems: 'center',
-          backgroundColor: 'transparent',
-        }} behavior='height'>
-          <View style={{
-            marginBottom: '5%',
-            height: '9%',
-            width: '90%',
-            backgroundColor: 'transparent', 
-            flexDirection: 'row',
-            display: 'none',
-          }}>
-            <Text style={{
-                fontSize: 17,
-                color: 'rgba(0,0,0,0.2)',
-                marginRight: '1%',
-            }}>keywordOne +</Text>
-            <Text style={{
-                fontSize: 17,
-                color: 'rgba(0,0,0,0.2)',
-                marginRight: '1%',
-            }}>keywordTwo+</Text>
-            <Text style={{
-                fontSize: 17,
-                color: 'rgba(0,0,0,0.2)',
-                marginRight: '1%',
-            }}>keywordThree +</Text>
-          </View>
-          <View style={{
-            height: '62.5%',
-            width: '90%',
-            //backgroundColor: 'red',
-            flexDirection: 'column',
-            borderStyle: 'solid',
-            borderColor: 'rgba(0,0,100,0.1)',
-            borderWidth: 2,
-            borderRadius: 10,
-          }}>
-            <TextInput 
-              style={[
-                styles.inputNote,
-                Platform.OS === 'web' ? {outlineStyle: 'none'} : {},
-                {
-                  backgroundColor: 'transparent',
-                  textAlignVertical: 'bottom',
-                  marginBottom: '2.5%',
-                  padding: 8.5,
-                  fontFamily: 'GeistRegular',
-              },
-              ]}
-              value={noteText}
-              placeholder='Just start typing...'
-              placeholderTextColor={'#909090'}
-              onChangeText={setNoteText}
-              autoFocus={true}
-              multiline={true} 
-            >
-            </TextInput>
-            <Animated.View style={[animatedStyle, {
-              marginTop: '1%',
-              height: '10%',
-              width: '103%',
-              alignItems: 'flex-end',
-            }]}>
-              <Pressable onPress={buttonAnimation} style={[{
-                  width: '15%',
-                  aspectRatio: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderRadius: 10,
-                  backgroundColor: 'rgba(90, 245, 90, 0.25)',
-              }]}>
-                <Text style={{
-                  fontSize: 18,
-                  color: 'rgba(10, 10, 10, 0.7)'
-                }}>{'\u2714'}</Text>
-              </Pressable>
-            </Animated.View>
-          </View>
-        </KeyboardAvoidingView>
-      </View>
-    </View>
-  );
+    const DARK_NOTE_TEXT_PLACEHOLDER = '#E6E6E6'
+    const LIGHT_NOTE_TEXT_PLACEHOLDER = '#5F5F5F'
+
+    const DARK_NOTE_TEXT = '#E6E6E6'
+    const LIGHT_NOTE_TEXT = '#1C1C1C'
+
+    const DARK_DONE_BUTTON_CHECKMARK = '#D4FFD0'
+    const LIGHT_DONE_BUTTON_CHECKMARK = '#222B21'
+
+    const DARK_BACKGROUND_DONE_BUTTON = '#222B21'
+    const LIGHT_BACKGROUND_DONE_BUTTON = '#F3F8F2'
+
+    const DARK_SETTINGS_BACK_BUTTON_ICON = '#444444'
+    const LIGHT_SETTINGS_BACK_BUTTON_ICON = '#E2E2E2'
+
+    const DARK_BACKGROUND_SETTINGS_BACK_BUTTON = '#444444'
+    const LIGHT_BACKGROUND_SETTINGS_BACK_BUTTON = '#F3F3F3'
+
+    const DARK_SETTINGS_BACKGROUND_CONTAINER = '#0F0F0F'
+    const LIGHT_SETTINGS_BACKGROUND_CONTAINER = '#FAFAFA'
+
+    const DARK_SETTINGS_CONTAINER_TEXT = '#DBDADA'
+    const LIGHT_SETTINGS_CONTAINTER_TEXT = '#292929'
+
+    const DARK_SETTINGS_HEADER = '#292929'
+    const LIGHT_SETTINGS_HEADER = '#DBDADA'
+
+    const DARK_BACKGROUND_NOTE_DONE = '#0C0C0C';
+    const LIGHT_BACKGROUND_NOTE_DONE = '#F3F8F2';
+
+    const LIGHT_SPACER  = '#F2F2F6';
+    const DARK_SPACER = '#151515';
+
+    const [isDarkModeActive, setIsDarkModeActive] = useState(false);
+
+    const {height, width} = useWindowDimensions();
+    const [logoWrapperWrapperHeight, setLogoWrapperHeight] = useState(height * 0.125);
+    const [noteTitleAndButtonHeight, setNoteTitleAndButtonHeight] = useState(height * 0.08);
+    const [spacerWrapperHeight, setSpacerWrapperHeight] = useState(height * 0.03)
+
+
+    //setStatusBarBackgroundColor('rgba(255,255,255,0.5)');
+    // listen to dimension change
+    useEffect(() => {
+    }, []);
+
+    return (
+        <View style={[styles.appContainer, {
+            backgroundColor: isDarkModeActive ? '#222120' : '#FFF9F3',
+            overflow: 'hidden'
+        }]}>
+                <View style={[styles.logoWrapperWrapper, {
+                        height: logoWrapperWrapperHeight,
+                        alignItems: 'center',
+                        justifyContent: 'flex-end'
+                    }]}>
+                    <View style={[styles.logoWrapper, {
+                        height: logoWrapperWrapperHeight * (Platform.OS === 'ios' ? 0.38 : 0.47),
+                    }]}>
+                        <Text style={[styles.logoText, {
+                            opacity: 0.05,
+                            color: isDarkModeActive ? DARK_LOGO : LIGHT_LOGO,
+                        }]}
+                            numberOfLines={1}
+                            adjustsFontSizeToFit
+                        >QckNote</Text>
+                    </View>
+                    <BlurView intensity={Platform.OS === 'ios' ? 8 : 4} style={[styles.blurLogoText,{
+                            // If there is lag, check this BlurView
+                    }]}>
+                    </BlurView>
+                </View>
+                <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'height' : 'height'}
+                    style={[styles.cardWrapper,
+                    {
+                        height: '87.5%',
+                        width: '100%',
+                    }
+                ]}>
+                    <View
+                        style={[styles.card, {
+                            height: '90%',
+                            backgroundColor: isDarkModeActive ? DARK_CARD : LIGHT_CARD,
+                        }]}>
+                        <View style={[styles.noteTitleAndButtonWrapper, {
+                            height: noteTitleAndButtonHeight,
+                        }]}>
+                            <View style={[styles.noteTitleWrapper, {
+                            }]}>
+                                <TextInput placeholder='Optional title..' 
+                                    placeholderTextColor={isDarkModeActive ? DARK_NOTE_TITLE_PLACEHOLDER : LIGHT_NOTE_TITLE_PLACEHOLDER} 
+                                    style={[styles.noteTitle, {
+                                        marginTop: '5%',
+                                        height: '100%',
+                                        fontSize: noteTitleAndButtonHeight * 0.36,
+                                        left: '-1%',
+                                        color: isDarkModeActive ? DARK_NOTE_TITLE : LIGHT_NOTE_TITLE,
+                                    }]}
+                                ></TextInput>
+                            </View>
+                            <View style={[styles.settingsButtonWrapper,
+                                {
+                                }
+                            ]}>
+                                <Pressable style={[styles.settingsButton,
+                                    {
+                                    }
+                                ]}>
+                                    <Image source={require('./assets/SettingsIcon.png')} style={[styles.settingsButtonIcon, {
+                                    }]}></Image>
+                                </Pressable>
+                            </View>
+                        </View>
+                        <View style={[styles.spacerWrapper,
+                            {
+                                height: spacerWrapperHeight,
+                            }
+                        ]}>
+                            <View style={[styles.spacer,
+                                {
+                                    backgroundColor: isDarkModeActive ? DARK_SPACER : LIGHT_SPACER,
+                                }
+                            ]}>
+                            </View>
+                        </View>
+                        <View style={[styles.noteTextWrapper, {
+                        }]}
+                        >
+                            <TextInput placeholderTextColor={isDarkModeActive ? DARK_NOTE_TEXT_PLACEHOLDER : LIGHT_NOTE_TEXT_PLACEHOLDER} 
+                                placeholder='Just start typing..' multiline={true}
+                                style={[styles.noteText,
+                                    {
+                                        fontSize: 20, //need to fix this
+                                        color: isDarkModeActive ? DARK_NOTE_TEXT : LIGHT_NOTE_TEXT,
+                                    }
+                                ]}
+                            >
+                            </TextInput>
+                        </View>
+                        <View style={[
+                            {
+                                position: 'absolute',
+                                width: '100%',
+                                height: '10%',
+                                top: '90%',
+                                alignItems: 'flex-end',
+                                justifyContent: 'flex-end',
+                            }
+                        ]}>
+                            <Pressable style={[styles.validateButtonWrapperWrapper, {
+                                // need to fix this
+                                height: 90,
+                                width: 90,
+                            }]}>
+                                <View  style={[styles.validateButtonWrapper,
+                                    {
+                                        borderRadius: 20,
+                                        height: '80%',
+                                        width: '80%',
+                                        backgroundColor: isDarkModeActive ? DARK_BACKGROUND_DONE_BUTTON : LIGHT_BACKGROUND_DONE_BUTTON,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    } 
+                                ]}>
+                                    {isDarkModeActive ? 
+                                        <Image source={require('./assets/DarkButtonCheckmark.png')}
+                                            style={[styles.validateButton]}
+                                        >
+                                        </Image>:
+                                        <Image source={require('./assets/WhiteButtonCheckmark.png')}
+                                            style={[styles.validateButton]}
+                                        >
+                                        </Image>
+                                    }
+                                </View>
+                            </Pressable>
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-  },
-  headerSettingsButtonWrapper: {
-    flexDirection: 'row',
-    height: '10%',
-    width: '90%',
-    marginbottom: '5%',
-  },
-  inputHeader: {
-    height: '70%',
-    width: '80%',
-    fontSize: 20,
-    borderWidth: 0,
-    padding: 8.5,
-  },
-  inputNote: {
-    height: '100%',
-    width: '96%',
-    fontSize: 18,
-  },
+    appContainer: {
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+    },
+    cardWrapper: {
+        alignItems: 'center',
+    },
+    card: {
+        width: '90%',
+        borderRadius: 20,
+        alignItems: 'center',
+    },
+    logoWrapperWrapper: {
+        width: '100%',
+    },
+    logoWrapper: {
+        marginBottom: '2%',
+        width: '100%',
+    },
+    logoText: {
+        fontStyle: 'italic',
+        fontSize: 1000,
+        textAlign: 'center',
+    },
+    blurLogoText: {
+        height: '100%',
+        width: '100%',
+        position: 'absolute',
+    },
+    noteTitleAndButtonWrapper: {
+        flexDirection: 'row',
+        height: '10%',
+        width: '90%',
+    },
+    noteTitleWrapper: {
+        height: '100%',
+        width: '80%',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+    },
+    settingsButtonWrapper: {
+        marginTop: '2%',
+        width: '20%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+    settingsButton: {
+        left: '5%',
+        height: '60%',
+        aspectRatio: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    settingsButtonIcon: { 
+        height: '90%',
+        width: '90%',
+        opacity: 0.8,
+    },
+    spacerWrapper: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    spacer: {
+        width: '86%',
+        height: '20%',
+        borderRadius: 10,
+    },
+    noteTextWrapper: {
+        marginTop: '4%',
+        height: '60%',
+        width: '90%',
+    },
+    noteText: {
+        width: '100%',
+    },
+    validateButton: {
+        height: '42%',
+        width: '42%',
+        resizeMode: 'contain',
+    }
 });
